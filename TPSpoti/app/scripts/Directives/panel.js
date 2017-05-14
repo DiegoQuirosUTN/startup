af04caf4dc6190a5cd0cf0bbd7b477fb51dd41a8
DiegoQuirosUTN <diegoaquiros@hotmail.com>
2017-05-13 23:33:03 -0300

    Directives for panels

---
+++ TPSpoti/app/scripts/Directives/panel.js
@@ -0,0 +1,20 @@
+angular.module('spotiApp')
+ .directive('panel', function() {
+    
+
+
+    return {
+      restrict: 'EA',
+      transclude: {
+        'title': 'panelTitle',
+        'body': 'panelBody',
+        'footer': 'panelFooter'
+      },
+      templateUrl: 'views/panelview.html'
+      /*template: '<div style="border: 1px solid black;">' +
+                  '<div class="title" ng-transclude="title">Fallback Title</div>' +
+                  '<div ng-transclude="body"></div>' +
+                  '<div class="footer" ng-transclude="footer">Fallback Footer</div>' +
+                '</div>'*/
+    };
+})
