b072795434c83616c2c416b27a1aedcd0716ada7
DiegoQuirosUTN <diegoaquiros@hotmail.com>
2017-05-15 00:12:33 -0300

    cleaning routes

--- TPSpoti/app/scripts/app.js
+++ TPSpoti/app/scripts/app.js
@@ -29,57 +29,27 @@ angular
     	.state('home', {
             url: '/home',
             templateUrl: '/views/homeview.html',
-            controller: 'homeCtrl',
-            ncyBreadcrumb: {
-   				label: 'Home '
-  			}
+            controller: 'homeCtrl'
         })
         
        
         .state('search', {
             url: '/search/:search',
             templateUrl: '/views/searchview.html',
-            controller: 'searchCtrl',
-            ncyBreadcrumb: {
-   				label: 'Search',
-   				parent: 'home'
-  			},
-            resolve:{
-     		 	artistId: ['$stateParams', function($stateParams){
-          			return $stateParams.search;
-      			}]
-   			}
+            controller: 'searchCtrl'
         })
         
       
         .state('artist', {
             url: '/artist/:artistId',
             templateUrl: '/views/artistview.html',
-            controller: 'artistCtrl',
-            ncyBreadcrumb: {
-   				label: 'artist',
-   				parent: "search({search: ''})"
-  			},
-            resolve:{
-      			artistId: ['$stateParams', function($stateParams){
-          			return $stateParams.artistId;
-      			}]
-   			}
+            controller: 'artistCtrl'
         })
 
 		.state('album', {
             url: '/album/:albumId',
             templateUrl: '/views/albumview.html',
-            controller: 'albumCtrl',
-            ncyBreadcrumb: {
-   				label: 'Album',
-   				parent: 'artist'
-  			},
-            resolve:{
-      			albumId: ['$stateParams', function($stateParams){
-          			return $stateParams.albumId;
-      			}]
-   			}
+            controller: 'albumCtrl'
         })
         ;
 
